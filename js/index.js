58b33b353153d118e115a814b04a20f822aacdfd
memoryza <jincai.wang@foxmail.com>
2014-10-14 13:19:09 +0800

    add detect page

---
+++ js/index.js
@@ -0,0 +1,98 @@
+$(function() {
+
+    $('#deviceInfo').text($.os.phone ? '手持设备' : ($.os.tablet ? '平板': 'pc'));
+    var osInfo = '未知';
+    if($.os.phone || $.os.tablet) {
+        if($.os.ios) {
+            if($.os.ipad) {
+                osInfo = 'ios系统(ipad) 版本:' + $.os.version;
+            } else if($.os.iphone) {
+                osInfo = 'ios系统(iphone) 版本:' + $.os.version;
+            } else {
+                osInfo = 'ios系统(ipod) 版本:' + $.os.version;
+            }
+        } else if($.os.blackberry) {
+            osInfo = '黑莓智能系统(BlackBerry) ,版本:' + $.os.version;
+        }  else if ($.os.rimtabletos) {
+            osInfo = 'BlackBerry Tablet OS系统(PlayBook) ,版本:' + $.os.version;
+        } else if($.os.bb10){
+            osInfo = '黑莓智能系统(bb10) ,版本:' + $.os.version +'<a href="http://baike.baidu.com/view/8500602.htm?fr=aladdin">详情</a>';
+        } else if($.os.android) {
+            osInfo = 'android ,版本:' + $.os.version;
+        } else if($.os.wp) {
+            osInfo = 'wp系统 ,版本:' + $.os.version;
+        } else if($.os.symbian) {
+            osInfo = 'symbian系统 ,版本:' + $.os.version;
+        } else if($.os.webos && $.os.touchpad){
+            osInfo = 'webos系统(touchpad),版本:' + $.os.version;
+        } else if($.os.webos) {
+            osInfo = 'webos系统,版本:' + $.os.version;
+        } else if($.os.kindle) {
+            osInfo = 'kindle,版本:' + $.os.version;
+        }
+    }
+    $('#osInfo').html(osInfo);
+    var browserInfo = '未知';
+    if($.browser.webkit) {
+        if($.browser.safari) {
+            osInfo = 'webkit内核(safari),版本:' + $.browser.version;
+        } else if($.browser.chrome) {
+             osInfo = 'webkit内核(chrome),版本:' + $.browser.version;
+        } else {
+            osInfo = 'webkit内核,版本:' + $.browser.version;
+        }
+    } else if($.browser.ie){
+        osInfo = 'Trident内核(ie),版本:' + $.browser.version;
+    }
+    $('#browserInfo').html(osInfo);
+
+    var shellInfo = '否';
+    if($.shell.isShell) {
+        if($.shell.weibo) {
+            shellInfo = '是(微博)';
+        } else if($.shell.weixin) {
+            shellInfo = '是(微信)';
+        }
+    }
+    $('#shellInfo').text(shellInfo);
+
+    $('#orientationSupport').text($.support.orientation ? '支持' : '不支持');
+    $('#defaultInnerHeight').text(window.innerHeight + '(innerHeight);' + $(window).height() +'(docheight)');
+    $('#defaultInnerWidth').text(window.innerWidth + '(innerWidth);' + $(window).width() +'(docWidth)');
+    $('#defaultOrientationAngle').text(window.orientation);
+
+    var portrait_map = {0: true, 180:true};
+    var isPortrait = true, elem = document.documentElement;
+    if($.support.orientation) {
+        isPortrait = portrait_map[ window.orientation ];
+    } else {
+        isPortrait = elem && elem.clientWidth / elem.clientHeight < 1.1;
+    }
+    $('#defaultorientationStatus').text(isPortrait ? '竖屏': '横屏');
+
+
+    var test = {
+        init: function() {
+            $('#orientationAngle').text(window.orientation);
+            if($.support.orientation) {
+                isPortrait = portrait_map[ window.orientation ];
+            } else {
+                isPortrait = elem && elem.clientWidth / elem.clientHeight < 1.1;
+            }
+            $('#orientationStatus').text(isPortrait ? '竖屏': '横屏');
+        }
+    }
+    if(window.orientation !== undefined) {
+        $(window).on('orientationchange', function(event) {
+            test.init(event);                
+        });     
+    } else if(screen.orientation !== undefined){
+        $(screen).on('orientationchange', function(event) {
+            test.init(event);                
+        })
+    } else {
+        $(window).on('resize', function(event) {
+            test.init(event);                
+        });     
+    }
+});
---
+++ js/z.detect.js
@@ -0,0 +1,92 @@
+;(function($){
+      function detect(ua){
+        var os = this.os = {}, browser = this.browser = {},
+          webkit = ua.match(/Web[kK]it[\/]{0,1}([\d.]+)/),
+          android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
+          osx = !!ua.match(/\(Macintosh\; Intel /),
+          ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+          ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/),
+          iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+          webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/),
+          wp = ua.match(/Windows Phone ([\d.]+)/),
+          touchpad = webos && ua.match(/TouchPad/),
+          kindle = ua.match(/Kindle\/([\d.]+)|Silk\/([\d._]+)/),
+          silk = ua.match(/Silk\/([\d._]+)/),
+          blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/),
+          bb10 = ua.match(/(BB10).*Version\/([\d.]+)/),
+          rimtabletos = ua.match(/(RIM\sTablet\sOS)\s([\d.]+)/),
+          playbook = ua.match(/PlayBook/),
+          chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/),
+          firefox = ua.match(/Firefox\/([\d.]+)/),
+          ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/),
+          webview = !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/),
+          safari = webview || ua.match(/Version\/([\d.]+)([^S](Safari)|[^M]*(Mobile)[^S]*(Safari))/)
+          symbian = ua.match(/SymbianOS\/([\d.]+)/);
+        // Todo: clean this up with a better OS/browser seperation:
+        // - discern (more) between multiple browsers on android
+        // - decide if kindle fire in silk mode is android or not
+        // - Firefox on Android doesn't specify the Android version
+        // - possibly devide in os, device and browser hashes
+
+        if (browser.webkit = !!webkit) browser.version = webkit[1]
+
+        if (android) os.android = true, os.version = android[2]
+        if (iphone && !ipod) os.ios = os.iphone = true, os.version = iphone[2].replace(/_/g, '.')
+        if (ipad) os.ios = os.ipad = true, os.version = ipad[2].replace(/_/g, '.')
+        if (ipod) os.ios = os.ipod = true, os.version = ipod[3] ? ipod[3].replace(/_/g, '.') : null
+        if (wp) os.wp = true, os.version = wp[1]
+        if (webos) os.webos = true, os.version = webos[2]
+        if (touchpad) os.touchpad = true
+        if (blackberry) os.blackberry = true, os.version = blackberry[2]
+        if (bb10) os.bb10 = true, os.version = bb10[2]
+        if (rimtabletos) os.rimtabletos = true, os.version = rimtabletos[2]
+        if(symbian) os.symbian = true, os.version = symbian[1]
+
+        if (playbook) browser.playbook = true
+        if (kindle) os.kindle = true, os.version = kindle[1] || kindle[2]
+        if (silk) browser.silk = true, browser.version = silk[1]
+        if (!silk && os.android && ua.match(/Kindle Fire/)) browser.silk = true
+        if (chrome) browser.chrome = true, browser.version = chrome[1]
+        if (firefox) browser.firefox = true, browser.version = firefox[1]
+        if (ie) browser.ie = true, browser.version = ie[1]
+        if (safari && (osx || os.ios)) {browser.safari = true; if (osx) browser.version = safari[1]}
+        if (webview) browser.webview = true
+  
+
+        os.tablet = !!(ipad || playbook || kindle || os.symbian || (android && !ua.match(/Mobile/)) ||
+          (firefox && ua.match(/Tablet/)) || (ie && !ua.match(/Phone/) && ua.match(/Touch/)))
+        os.phone  = !!(!os.tablet && !os.ipod&& (android || iphone || webos || blackberry || bb10 ||
+          (chrome && ua.match(/Android/)) || (chrome && ua.match(/CriOS\/([\d.]+)/)) ||
+          (firefox && ua.match(/Mobile/)) || (ie && ua.match(/Touch/))))
+      }
+
+      detect.call($, navigator.userAgent)
+      // make available to unit tests
+      $.__detect = detect
+
+    })(Zepto);
+    (function($) {
+         /**
+             * @name $.support
+             * - ***orientation*** 检测是否支持转屏事件，UC中存在orientaion，但转屏不会触发该事件，故UC属于不支持转屏事件(iOS 4上qq, chrome都有这个现象)
+        */
+        var br  =  $.browser;
+        $.support = $.extend($.support || {}, {
+            orientation: !(br.uc || (parseFloat($.os.version) < 5 && (br.qq || br.chrome))) && !($.os.android && parseFloat($.os.version) > 3) && "orientation" in window && "onorientationchange" in window
+        });
+    })(Zepto);
+    (function($) {
+        /**
+        * @name $.support
+        * 是否嵌套壳
+        */
+        function detectShell(ua) {
+            var shell = this.shell = {},
+                weibo = ua.toLowerCase().match(/_weibo_/i),
+                weixin = ua.match(/MicroMessenger/i);
+            if(weibo) shell.weibo = true;
+            if(weixin) shell.weixin = true;
+            shell.isShell = !!(weixin || weibo);
+        }
+        detectShell.call($,  navigator.userAgent);
+    })(Zepto);
